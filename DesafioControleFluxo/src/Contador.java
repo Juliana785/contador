42f74b7e011415bbce67295c324e71ca52177a1d
Juliana Capovilla <capovilla.ttg@gmail.com>
2023-09-29 23:44:27 -0300

    desafio controle fluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+import javax.net.ssl.ExtendedSSLSession;
+
+
+public class Contador {
+    
+    public static void main(String[] args) throws  Exception {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+       
+
+        try{
+            contar (parametroUm, parametroDois);
+
+            }
+            catch(ParametrosInvalidosException exception){
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro: ");
+            }
+    }
+    
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }else {
+            int contagem = parametroDois - parametroUm;
+            for (int contador = 0; contador <= contagem; contador++){
+                System.out.println("Contagem" + contador);
+            }
+        }
+    }
+}
